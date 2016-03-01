33f6dd75da2da086a7c01981374d0bd73befff7a
fanyongdong <fanyongdong@xiaomi.com>
2016-03-02 00:20:12 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+
+var app = express();
+
+var env = app.get('env');
+
+var config = require('./config')[env];
+
+//config variable
+global.config = config;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes.index);
+
+
---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+  "development": {
+    "server_port": 3001
+  },
+  "production": {
+    "server_port": 3000
+  }
+};
+module.exports = config;
+
